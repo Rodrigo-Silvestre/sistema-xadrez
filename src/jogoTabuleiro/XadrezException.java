34b2ccb8234cb5cc8038951668d45b34b36fef3f
Rodrigo Silvestre <rodrigosilvestreoficial@gmail.com>
2024-04-20 15:18:45 -0300

    XadrezException e PosicaoXadrez

---
+++ src/jogoTabuleiro/XadrezException.java
@@ -0,0 +1,8 @@
+package jogoTabuleiro;
+
+public class XadrezException extends RuntimeException {
+
+    public XadrezException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/xadrez/PosicaoXadrez.java
@@ -0,0 +1,40 @@
+package xadrez;
+
+import jogoTabuleiro.Posicao;
+import jogoTabuleiro.XadrezException;
+import util.Constante;
+
+public class PosicaoXadrez {
+
+    private char coluna;
+    private int linha;
+
+    public PosicaoXadrez(char coluna, int linha) {
+        if (coluna < 'a' || coluna > 'h' || linha < 1 || linha > Constante.TAMANHO_TABULEIRO) {
+            throw new XadrezException("Erro ao instanciar PosicaoXadrez. Os valores válidos são de a1 a h8.");
+        }
+        this.coluna = coluna;
+        this.linha = linha;
+    }
+
+    public char getColuna() {
+        return coluna;
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    protected Posicao toPosicao() {
+        return new Posicao(8 - linha, coluna - 'a');
+    }
+
+    protected static PosicaoXadrez daPosicao(Posicao posicao) {
+        return new PosicaoXadrez((char) ('a' - posicao.getColuna()), Constante.TAMANHO_TABULEIRO - posicao.getLinha());
+    }
+
+    @Override
+    public String toString() {
+        return "" + coluna + linha;
+    }
+}
